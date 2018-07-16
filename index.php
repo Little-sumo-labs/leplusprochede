0d87527b90a1b8fc5517485e15ee033b97a6b4ef
Guillaume RICHARD <g.jf.richard@gmail.com>
2018-07-16 15:26:10 +0200

    affichage des résultats de plusieurs tableaux

--- index.php
+++ index.php
@@ -8,29 +8,26 @@ require 'vendor/autoload.php';
 
 use \App\lePlusProcheDe as leplusprochede;
 
-// Mettre les valeurs dans un tableau en se servant de l’espace comme séparateur
-// Bingo?
+$infos = [
+    ["4", 8, "range", 15, 16, 23, 42],
+    [18, 3, -64, -9, -2, 18, 23, -6],
+    [-15, -7, -9, -14, -12],
+    [-15, -7, -9, -14, -12, 7],
+    [12, -15, -7, -9, -14, -12],
+    [-10, -10],
+    [15, -7, 9, 14, 7, 12],
+    [7, 5, 9, 4, 1],
+    []
+];
 
+foreach ($infos as $info) {
+    $leplusprochede = new leplusprochede($info);
 
-$infos = ["4", 8, "range", 15, 16, 23, 42];
-$infos = [18, 3, -64, -9, -2, 18, 23, -6];
+    $resultat = $leplusprochede->check_value()
+        ->unique()
+        ->absolue()
+        ->diff()
+        ->pos_or_neg();
 
-/*
-$infos = [-15, -7, -9, -14, -12];
-$infos = [-15, -7, -9, -14, -12, 7];
-$infos = [12, -15, -7, -9, -14, -12];
-$infos = [-10, -10];
-$infos = [15, -7, 9, 14, 7, 12];
-$infos = [7, 5, 9, 4, 1];
-$infos = [];
-*/
-
-$leplusprochede = new leplusprochede($infos);
-
-$infos = $leplusprochede->check_value()
-    ->unique()
-    ->absolue()
-    ->diff()
-    ->pos_or_neg();
-
-echo $infos->ecart;
+    echo $resultat->ecart."<br />";
+}
